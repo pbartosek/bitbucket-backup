23596e28f671a902acf523244570fc0d724e65b3
Paul <pbartosek@gmail.com>
2019-11-18 20:30:31 -0700

    Working clone

--- bitbucket-backup.py
+++ bitbucket-backup.py
@@ -4,15 +4,11 @@ import configparser
 import requests
 import time
 import json
+import os
+import subprocess
 
 
-# import logging
-# logging.basicConfig()
-# log = logging.getLogger()
-# log.setLevel('DEBUG')
-
-
-class BitBucketAuth:
+class BitBucketAPI:
     api_uri = "https://api.bitbucket.org/2.0"
     client_id = None
     client_secret = None
@@ -20,6 +16,7 @@ class BitBucketAuth:
     parser = None
     filename = None
     username = None
+    repo_file = None
 
     def __init__(self, filename):
         self.filename = filename
@@ -28,6 +25,7 @@ class BitBucketAuth:
         self.client_id = self.parser.get('default', 'client_id')
         self.client_secret = self.parser.get('default', 'client_secret')
         self.username = self.parser.get('default', 'username')
+        self.repo_file = self.parser['default'].get('repo_file', 'repo_cache.json')
         if self.parser.has_option('default', 'token'):
             self.token = json.loads(self.parser.get('default', 'token'))
 
@@ -58,7 +56,10 @@ class BitBucketAuth:
             ret = requests.get(self.api_uri + uri, headers=headers)
         return ret
 
-    def get_repositories(self):
+    def get_repositories(self, force=False):
+        if os.path.exists(self.repo_file) and not force:
+            with open(self.repo_file) as f:
+                return json.load(f)
         repos = []
         pagenum = 1
         while True:
@@ -70,11 +71,41 @@ class BitBucketAuth:
             if 'next' in ret:
                 pagenum += 1
                 continue
+            with open(self.repo_file, 'w+') as f:
+                json.dump(repos, f, indent=2)
             return repos
 
+    def get_filtered_repositories(self):
+        ret = []
+        for r in self.get_repositories():
+            if r['scm'] != 'git':
+                continue
+            ret.append({
+                'url': [x['href'] for x in r['links']['clone'] if x['name'] == 'ssh'][0],
+                'name': r['name'],
+                'full_name': r['full_name'],
+                'updated': r['updated_on']
+            })
+        return ret
 
-bb = BitBucketAuth('config.ini')
 
-rep = bb.get_repositories()
+def backup_repository(repo):
+    url = repo['url']
+    backup_dir = os.path.join('backup', repo['full_name'])
+    opts = ['git', 'clone', url, backup_dir]
+    print(' '.join(opts))
+    subprocess.run(opts)
+    quit()
 
-print(json.dumps(rep, indent=2))
+
+def backup_repositories(repos):
+    for r in repos:
+        backup_repository(r)
+
+
+bb = BitBucketAPI('config.ini')
+
+repos = bb.get_filtered_repositories()
+backup_repositories(repos)
+
+# print(json.dumps(repos, indent=2))
